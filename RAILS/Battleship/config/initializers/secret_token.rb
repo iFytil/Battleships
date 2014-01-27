7c6608641139826fdd0d8a53f759a9171d35f7c5
Toby <tlornewr@gmail.com>
2014-01-27 16:51:12 -0500

    rails pls

---
+++ RAILS/Battleship/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Battleship::Application.config.secret_key_base = '826abdb145769e1aa8aea05b3a73202e0a2651167afa36be88fbec659b80e2fd8654061ce456d1624447e39068363df6bc9a7ae9bb831376abbea35d23e4a0e1'
---
+++ UI/README.txt
@@ -0,0 +1,7 @@
+Notes
+  Color scheme in mockups is not actual color scheme.
+  Color palette needs to be discussed.
+
+UI Attributes
+  pagewidth: 900px
+  navigationwidth: pagewidth
