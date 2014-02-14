e4b221bc024b4c99d2c90c490ba3465eeffeda50
Ioannis Fytilis <ioannis.fytilis@hotmail.com>
2014-02-14 17:24:12 -0500

    Adding fields to a table using rails generate migration

--- RAILS/Battleship/db/schema.rb
+++ RAILS/Battleship/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140127213043) do
+ActiveRecord::Schema.define(version: 20140214222307) do
 
   create_table "rails_admin_histories", force: true do |t|
     t.text     "message"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20140127213043) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "wins"
+    t.integer  "losses"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
