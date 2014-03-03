063f75869ba2ec05242bb0b6894f545d0fa7fbd2
Ioannis Fytilis <ioannis.fytilis@hotmail.com>
2014-03-03 15:23:01 -0500

    Added list of current games for each player

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,5 @@
 module GamesHelper
+  def all_my_games(id)
+    Game.where(player_1: id) + Game.where(player_2: id)
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Battleship
     # -- all .rb files in that directory are automatically loaded.
 
     config.assets.precompile += %w( chat.js )
+    config.assets.precompile += %w( games.js )
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
