a660f276224c0ee0a8ae588f473ff5c0c5e3407b
Ioannis Fytilis <ioannis.fytilis@hotmail.com>
2014-03-04 20:34:53 -0500

    Moved game CSS to appropriate file

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Battleship
     config.assets.precompile += %w( chat.js )
     config.assets.precompile += %w( chat.css )
     config.assets.precompile += %w( games.js )
+    config.assets.precompile += %w( games.css )
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
