e4806a1493dd8d22975c820d81a5cb80d7f815b1
Stephanie <stephanie@ubuntu.(none)>
2014-03-09 14:34:32 -0400

    added fields for specifying ships' radar and cannon zones

--- db/migrate/20140305063215_create_shiptypes.rb
+++ db/migrate/20140305063215_create_shiptypes.rb
@@ -4,6 +4,12 @@ class CreateShiptypes < ActiveRecord::Migration
       t.integer :size
       t.integer :speed
       t.integer :armor
+      t.integer :radar_back
+      t.integer :radar_w
+      t.integer :radar_l
+      t.integer :cannon_back
+      t.integer :cannon_w
+      t.integer :cannon_l
       t.string :name
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Shiptype.create(:size => 5, :speed => 10, :armor => 2, :name => "Cruiser")
-Shiptype.create(:size => 4, :speed => 8, :armor => 1, :name => "Destroyer")
-Shiptype.create(:size => 3, :speed => 9, :armor => 1, :name => "Torpedo Boat")
-Shiptype.create(:size => 2, :speed => 6, :armor => 2, :name => "Mine Layer")
-Shiptype.create(:size => 3, :speed => 3, :armor => 1, :name => "Radar Boat")
+Shiptype.create(:radar_back => 1, :radar_w => 3, :radar_l => 10,:cannon_back => -5, :cannon_w => 11, :cannon_l => 15,:size => 5, :speed => 10, :armor => 2, :name => "Cruiser")
+Shiptype.create(:radar_back => 1, :radar_w => 3, :radar_l => 8,:cannon_back => -4, :cannon_w => 9, :cannon_l => 12,:size => 4, :speed => 8, :armor => 1, :name => "Destroyer")
+Shiptype.create(:radar_back => 1, :radar_w => 3, :radar_l => 6,:cannon_back => 0, :cannon_w => 5, :cannon_l => 5,:size => 3, :speed => 9, :armor => 1, :name => "Torpedo Boat")
+Shiptype.create(:radar_back => -2, :radar_w => 5, :radar_l => 6,:cannon_back => -1, :cannon_w => 5, :cannon_l => 4,:size => 2, :speed => 6, :armor => 2, :name => "Mine Layer")
+Shiptype.create(:radar_back => 1, :radar_w => 3, :radar_l => 6,:cannon_back => -1, :cannon_w => 3, :cannon_l => 5,:size => 3, :speed => 3, :armor => 1, :name => "Radar Boat")
