d8517c47a76598aea00ff2415a0a2ecb7adbc88f
Ioannis Fytilis <ioannis.fytilis@hotmail.com>
2014-03-12 13:45:06 -0400

    Less verbose function

--- app/assets/javascripts/games/components.js
+++ app/assets/javascripts/games/components.js
@@ -76,184 +76,124 @@ Ship = function (x, y, length, speed, facing, radar,cannon) {
   // spacing 
   var s = 4;
 
-this.Set = function(){
-  
-  if (this.facing == D.Right) 
-  {
-    this.points[0] =new Point(this.sternx+s,this.sterny+s);
-    this.points[1] =new Point(this.sternx+s,this.sterny+SQ_WIDTH-s);
-    this.points[2] =new Point(this.sternx+(length-1)*SQ_WIDTH,this.sterny+SQ_WIDTH-s);
-    this.points[3] =new Point(this.sternx+(length)*SQ_WIDTH-t,this.sterny+t);
-    this.points[4] =new Point(this.sternx+(length-1)*SQ_WIDTH,this.sterny+s);
-  } 
-  else if (this.facing == D.Left) 
-  {
-    this.points[0] =new Point(this.sternx+SQ_WIDTH-s,this.sterny+s);
-    this.points[1] =new Point(this.sternx+SQ_WIDTH-s,this.sterny+SQ_WIDTH-s);
-    this.points[2] =new Point(this.sternx-(length-2)*SQ_WIDTH,this.sterny+SQ_WIDTH-s);
-    this.points[3] =new Point(this.sternx-(length-1)*SQ_WIDTH+t,this.sterny+t);
-    this.points[4] =new Point(this.sternx-(length-2)*SQ_WIDTH,this.sterny+s);
-  } 
-  else if (this.facing == D.Up) 
-  {
-    this.points[0] =new Point(this.sternx+s,this.sterny+SQ_WIDTH-s);
-    this.points[1] =new Point(this.sternx+SQ_WIDTH-s,this.sterny+SQ_WIDTH-s);
-    this.points[2] =new Point(this.sternx+SQ_WIDTH-s,this.sterny-(length-2)*SQ_WIDTH);
-    this.points[3] =new Point(this.sternx+t,this.sterny-(length-1)*SQ_WIDTH+t);
-    this.points[4] =new Point(this.sternx+s,this.sterny-(length-2)*SQ_WIDTH);
-  } 
-  else if (this.facing == D.Down) 
-  {
-    this.points[0] =new Point(this.sternx+s,this.sterny+s);
-    this.points[1] =new Point(this.sternx+SQ_WIDTH-s,this.sterny+s);
-    this.points[2] =new Point(this.sternx+SQ_WIDTH-s,this.sterny+(length-1)*SQ_WIDTH);
-    this.points[3] =new Point(this.sternx+t,this.sterny+(length)*SQ_WIDTH-t);
-    this.points[4] =new Point(this.sternx+s,this.sterny+(length-1)*SQ_WIDTH);
-  }
-}
-this.Set();
-
-  this.Forward = function (a) {
-    if (this.facing == D.Right) {
-        for (var i = 0; i < 5; i++) {
-          this.points[i].x += SQ_WIDTH * a;
-        }
-    } else if (this.facing == D.Left) {
-        for (var i = 0; i < 5; i++) {
-          this.points[i].x -= SQ_WIDTH * a;
-        }
-    } else if (this.facing == D.Up) {
-        for (var i = 0; i < 5; i++) {
-          this.points[i].y -= SQ_WIDTH * a;
-        }
-    } else if (this.facing == D.Down) {
-        for (var i = 0; i < 5; i++) {
-          this.points[i].y += SQ_WIDTH * a;
-        }
+  this.Set = function(){
+    
+    if (this.facing == D.Right) 
+    {
+      this.points[0] =new Point(this.sternx+s,this.sterny+s);
+      this.points[1] =new Point(this.sternx+s,this.sterny+SQ_WIDTH-s);
+      this.points[2] =new Point(this.sternx+(length-1)*SQ_WIDTH,this.sterny+SQ_WIDTH-s);
+      this.points[3] =new Point(this.sternx+(length)*SQ_WIDTH-t,this.sterny+t);
+      this.points[4] =new Point(this.sternx+(length-1)*SQ_WIDTH,this.sterny+s);
+    } 
+    else if (this.facing == D.Left) 
+    {
+      this.points[0] =new Point(this.sternx+SQ_WIDTH-s,this.sterny+s);
+      this.points[1] =new Point(this.sternx+SQ_WIDTH-s,this.sterny+SQ_WIDTH-s);
+      this.points[2] =new Point(this.sternx-(length-2)*SQ_WIDTH,this.sterny+SQ_WIDTH-s);
+      this.points[3] =new Point(this.sternx-(length-1)*SQ_WIDTH+t,this.sterny+t);
+      this.points[4] =new Point(this.sternx-(length-2)*SQ_WIDTH,this.sterny+s);
+    } 
+    else if (this.facing == D.Up) 
+    {
+      this.points[0] =new Point(this.sternx+s,this.sterny+SQ_WIDTH-s);
+      this.points[1] =new Point(this.sternx+SQ_WIDTH-s,this.sterny+SQ_WIDTH-s);
+      this.points[2] =new Point(this.sternx+SQ_WIDTH-s,this.sterny-(length-2)*SQ_WIDTH);
+      this.points[3] =new Point(this.sternx+t,this.sterny-(length-1)*SQ_WIDTH+t);
+      this.points[4] =new Point(this.sternx+s,this.sterny-(length-2)*SQ_WIDTH);
+    } 
+    else if (this.facing == D.Down) 
+    {
+      this.points[0] =new Point(this.sternx+s,this.sterny+s);
+      this.points[1] =new Point(this.sternx+SQ_WIDTH-s,this.sterny+s);
+      this.points[2] =new Point(this.sternx+SQ_WIDTH-s,this.sterny+(length-1)*SQ_WIDTH);
+      this.points[3] =new Point(this.sternx+t,this.sterny+(length)*SQ_WIDTH-t);
+      this.points[4] =new Point(this.sternx+s,this.sterny+(length-1)*SQ_WIDTH);
     }
   }
-  this.Backward = function () {
-      if (this.facing == D.Right) {
-          for (var i = 0; i < 5; i++) {
-              this.points[i].x -= SQ_WIDTH;
-          }
-      } else if (this.facing == D.Left) {
-          for (var i = 0; i < 5; i++) {
-              this.points[i].x += SQ_WIDTH;
-          }
-      } else if (this.facing == D.Up) {
-          for (var i = 0; i < 5; i++) {
-              this.points[i].y += SQ_WIDTH;
-          }
-      } else if (this.facing == D.Down) {
-          for (var i = 0; i < 5; i++) {
-              this.points[i].y -= SQ_WIDTH;
-          }
-      }
-  }
+  this.Set();
+
   this.Left = function () {
-      for (var i = 0; i < 5; i++) {
-              this.points[i].x -= SQ_WIDTH;
-              
-          }
+    for (var i = 0; i < 5; i++) {
+      this.points[i].x -= SQ_WIDTH;            
+    }
   }
   this.Right = function () {
-      for (var i = 0; i < 5; i++) {
-              this.points[i].x += SQ_WIDTH;
-          }
+    for (var i = 0; i < 5; i++) {
+      this.points[i].x += SQ_WIDTH;
+    }
   }
  
   this.Up = function () {
-      for (var i = 0; i < 5; i++) {
-              this.points[i].y -= SQ_WIDTH;
-          }
+    for (var i = 0; i < 5; i++) {
+      this.points[i].y -= SQ_WIDTH;
+    }
   }
   
   this.Down = function () {
-      for (var i = 0; i < 5; i++) {
-              this.points[i].y += SQ_WIDTH;
-          }
-  }
-  this.CW = function () {
-    if (this.facing == D.Right) {
-        this.facing = D.Down;
-        this.Set();
-    } else if (this.facing == D.Left) {
-        this.facing = D.Up;
-        this.Set();
-    } else if (this.facing == D.Up) {
-        this.facing = D.Right;
-        this.Set();
-    } else if (this.facing == D.Down) {
-        this.facing = D.Left;
-        this.Set();
+    for (var i = 0; i < 5; i++) {
+      this.points[i].y += SQ_WIDTH;
     }
   }
-  this.CCW = function () {
-      if (this.facing == D.Right) {
-        this.facing = D.Up;
-        this.Set();
-    } else if (this.facing == D.Left) {
-        this.facing = D.Down;
-        this.Set();
-    } else if (this.facing == D.Up) {
-        this.facing = D.Left;
-        this.Set();
-    } else if (this.facing == D.Down) {
-        this.facing = D.Right;
-        this.Set();
-    }
-  }
-    this.highlighted = false;
-    this.Draw = function (ctx, color) {
-        ctx.beginPath();
-        ctx.moveTo(this.points[0].x, this.points[0].y);
-        ctx.lineTo(this.points[1].x, this.points[1].y);
-        ctx.lineTo(this.points[2].x, this.points[2].y);
-        ctx.lineTo(this.points[3].x, this.points[3].y);
-        ctx.lineTo(this.points[4].x, this.points[4].y);
-        ctx.closePath();
-        ctx.lineWidth = 2;
-        if (this.highlighted)
-            ctx.strokeStyle = 'yellow';
-        else
-            ctx.strokeStyle = color;
-        ctx.stroke();
-        ctx.fillStyle = color;
-        ctx.fill();
-        
-        this.radarzone.Draw(ctx, 'yellow');
-        this.cannonzone.Draw(ctx, 'orange');
-    }
 
+  this.CW = function () {
+    this.facing = (this.facing + 1)%4
+    this.Set();
+  }
+
+  this.CCW = function () {
+    this.facing = (this.facing + 3)%4
+    this.Set();
+  }
+
+  this.highlighted = false;
+  this.Draw = function (ctx, color) {
+      ctx.beginPath();
+      ctx.moveTo(this.points[0].x, this.points[0].y);
+      ctx.lineTo(this.points[1].x, this.points[1].y);
+      ctx.lineTo(this.points[2].x, this.points[2].y);
+      ctx.lineTo(this.points[3].x, this.points[3].y);
+      ctx.lineTo(this.points[4].x, this.points[4].y);
+      ctx.closePath();
+      ctx.lineWidth = 2;
+      if (this.highlighted)
+          ctx.strokeStyle = 'yellow';
+      else
+          ctx.strokeStyle = color;
+      ctx.stroke();
+      ctx.fillStyle = color;
+      ctx.fill();
+      
+      this.radarzone.Draw(ctx, 'yellow');
+      this.cannonzone.Draw(ctx, 'orange');
+  }
 
 };
 
 Base = function (x, y) {
 
-    var x0 = x * SQ_WIDTH;
-    var y0 = y * SQ_WIDTH;
+  var x0 = x * SQ_WIDTH;
+  var y0 = y * SQ_WIDTH;
 
-    var points = new Array();
-    points.push(new Point(x0, y0));
-    points.push(new Point(x0 + SQ_WIDTH, y0));
-    points.push(new Point(x0 + SQ_WIDTH, y0 + SQ_WIDTH * 10));
-    points.push(new Point(x0, y0 + SQ_WIDTH * 10));
+  var points = new Array();
+  points.push(new Point(x0, y0));
+  points.push(new Point(x0 + SQ_WIDTH, y0));
+  points.push(new Point(x0 + SQ_WIDTH, y0 + SQ_WIDTH * 10));
+  points.push(new Point(x0, y0 + SQ_WIDTH * 10));
 
-    this.Draw = function (ctx, color) {
-        ctx.beginPath();
-        ctx.moveTo(points[0].x, points[0].y);
-        ctx.lineTo(points[1].x, points[1].y);
-        ctx.lineTo(points[2].x, points[2].y);
-        ctx.lineTo(points[3].x, points[3].y);
-        ctx.closePath();
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = color;
-        ctx.stroke();
-        ctx.fillStyle = color;
-        ctx.fill();
-    }
-
+  this.Draw = function (ctx, color) {
+    ctx.beginPath();
+    ctx.moveTo(points[0].x, points[0].y);
+    ctx.lineTo(points[1].x, points[1].y);
+    ctx.lineTo(points[2].x, points[2].y);
+    ctx.lineTo(points[3].x, points[3].y);
+    ctx.closePath();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = color;
+    ctx.stroke();
+    ctx.fillStyle = color;
+    ctx.fill();
+  }
 
 };
 
