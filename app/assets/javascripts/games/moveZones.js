8cfa6c9f03f36c6f4575f881058eb6a322625d7f
Gerald <some_1337@web110253.wireless.mcgill.ca>
2014-03-13 15:04:01 -0400

    Fixed brokenness of last commit

--- app/assets/javascripts/games/moveZones.js
+++ app/assets/javascripts/games/moveZones.js
@@ -1,4 +1,4 @@
-DrawRotationZone = function (ship, ctx){
+DrawRotationZone = function (ship){
 
 	if(ship.facing == D.Right) {
 		var lFace = D.Up; 
@@ -37,30 +37,57 @@ DrawRotationZone = function (ship, ctx){
 		var rYOffset = -ship.turnIndex;
 	}
 
+	var color = "rgba(255,255,0,100)";
+
 	if( ship.turnSpeed == 1 || ship.turnSpeed == 2)
 	{
 		// Draw left
 		var lShip = new Ship(ship.x + lXOffset, ship.y + lYOffset, ship.length, ship.speed, lFace, ship.radarzone, ship.cannonzone, ship.health, ship.armor, ship.turnSpeed, ship.turnIndex)
 
-		lShip.Draw(ctx, 'translucentYellow', false)
+		lShip.Draw(ctx, color, false)
+
+		// Draw left turn radius
+
+
 		// Draw right
 		var lShip = new Ship(ship.x + rXOffset, ship.y + rYOffset, ship.length, ship.speed, rFace, ship.radarzone, ship.cannonzone, ship.health, ship.armor, ship.turnSpeed, ship.turnIndex)
 
-		rShip.Draw(ctx, 'translucentYellow', false)
-	}
-	// if(ship.turnSpeed == 2)
-	// {
-	// 	if(ship.facing == D.Right) {
-	// 		var bFace = D.Left;
-	// 		var bXOffset = ;
-	// 		var bYOffset = ;
-	// 	}
-	// 	else if(ship.facing == D.Up) var bFace = D.Down
-	// 	else if(ship.facing == D.Left) var bFace = D.Right
-	// 	else if(ship.facing == D.Down) var bFace = D.Up
-	// 	// Draw behind
+		rShip.Draw(ctx, color, false)
 
-	// }
+		// Draw right turn radius
+
+	}
+	if(ship.turnSpeed == 2)
+	{
+		if(ship.facing == D.Right) {
+			var bFace = D.Left;
+			var bXOffset = ship.turnIndex * 2;
+			var bYOffset = 0;
+		}
+		else if(ship.facing == D.Up) {
+			var bFace = D.Down;
+			var bXOffset = 0;
+			var bYOffset = ship.turnIndex * 2;
+		}
+		else if(ship.facing == D.Left) {
+			var bFace = D.Right
+			var bXOffset = -ship.turnIndex * 2;
+			var bYOffset = 0;
+		}
+		else if(ship.facing == D.Down) {
+			var bFace = D.Up
+			var bXOffset = 0;
+			var bYOffset = -ship.turnIndex * 2;
+		}
+
+		// Draw behind
+		var bShip = new Ship(ship.x + bXOffset, ship.y + bYOffset, ship.length, ship.speed, bFace, ship.radarzone, ship.cannonzone, ship.health, ship.armor, ship.turnSpeed, ship.turnIndex)
+
+		bShip.Draw(ctx, color, false)
+
+		// Draw turn around radius
+
+	}
 
       	
 }
