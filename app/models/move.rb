b853cc9db1dae2f1c1c95af3c212b33f4a4f1818
Gerald <some_1337@web110246.wireless.mcgill.ca>
2014-03-14 03:25:38 -0400

    Rotate with turn index working

--- app/models/move.rb
+++ app/models/move.rb
@@ -25,9 +25,69 @@ class Move < ActiveRecord::Base
       end
 
     when "Rotate"
-      d1 = shipToDelta(ship,ship.shiptype.turn_index)
-      ship.direction = whereToRotate(move,ship)
-      d2 = shipToDelta(ship,ship.shiptype.turn_index)
+      # ship.direction = whereToRotate(move,ship)
+
+      # if ship.shiptype.turn_index != 0
+
+      # end
+      turn_index = ship.shiptype.turn_index
+      case ship.direction
+      when "Up"
+        if ship.location_x == move.pos_x && ship.location_y == move.pos_y
+          ship.direction = "Down"
+          ship.location_y -= turn_index * 2
+        elsif move.pos_x < ship.location_x 
+          ship.direction = "Left"
+          ship.location_x += turn_index
+          ship.location_y -= turn_index
+        else
+          ship.direction = "Right"
+          ship.location_x -= turn_index
+          ship.location_y -= turn_index
+        end
+
+      when "Down"
+        if ship.location_x == move.pos_x && ship.location_y == move.pos_y
+          ship.direction = "Up"
+          ship.location_y += turn_index * 2
+        elsif move.pos_x < ship.location_x 
+          ship.direction = "Left"
+          ship.location_x += turn_index
+          ship.location_y += turn_index
+        else
+          ship.direction = "Right"
+          ship.location_x -= turn_index
+          ship.location_y += turn_index
+        end
+
+      when "Left"
+        if ship.location_x == move.pos_x && ship.location_y == move.pos_y
+          ship.direction = "Right"
+          ship.location_x -= turn_index * 2
+        elsif move.pos_y < ship.location_y
+          ship.direction = "Up"
+          ship.location_x -= turn_index
+          ship.location_y += turn_index
+        else
+          ship.direction = "Down"
+          ship.location_x -= turn_index
+          ship.location_y -= turn_index
+        end 
+
+      when "Right"
+        if ship.location_x == move.pos_x && ship.location_y == move.pos_y
+          ship.direction = "Left"
+          ship.location_x += turn_index * 2
+        elsif move.pos_y < ship.location_y
+          ship.direction = "Up"
+          ship.location_x += turn_index
+          ship.location_y += turn_index
+        else
+          ship.direction = "Down"
+          ship.location_x += turn_index
+          ship.location_y -= turn_index
+        end
+      end
     end
 
     ship.save
@@ -39,21 +99,45 @@ class Move < ActiveRecord::Base
     x >= 10 && x < 20 && y >= 3 && y < 27 && game.coral[(y - 3)*10 + (x - 10)]=='1'
   end
 
-  def whereToRotate(move,ship)
-    if ship.location_x == move.pos_x
-      if ship.location_y > move.pos_y
-        "Up"
-      else
-        "Down"
-      end
-    else
-      if ship.location_x > move.pos_x
-        "Left"
-      else
-        "Right"
-      end
-    end
-  end
+  # def whereToRotate(move,ship)
+  #   case ship.direction
+  #   when "Up"
+  #     if ship.location_x == move.pos_x && ship.location_y == move.pos_y
+  #       "Down"
+  #     elsif move.pos_x < ship.location_x 
+  #       "Left"
+  #     else
+  #       "Right"
+  #     end
+
+  #   when "Down"
+  #     if ship.location_x == move.pos_x && ship.location_y == move.pos_y
+  #       "Up"
+  #     elsif move.pos_x < ship.location_x 
+  #       "Left"
+  #     else
+  #       "Right"
+  #     end
+
+  #   when "Left"
+  #     if ship.location_x == move.pos_x && ship.location_y == move.pos_y
+  #       "Right"
+  #     elsif move.pos_y < ship.location_y
+  #       "Up"
+  #     else
+  #       "Down"
+  #     end 
+
+  #   when "Right"
+  #     if ship.location_x == move.pos_x && ship.location_y == move.pos_y
+  #       "Left"
+  #     elsif move.pos_y < ship.location_y
+  #       "Up"
+  #     else
+  #       "Down"
+  #     end
+  #   end
+  # end
 
   def addToShip(delta)
     if !isCoral(ship.location_x + delta[:x], ship.location_y + delta[:y])
