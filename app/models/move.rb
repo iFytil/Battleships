a48bc3a4e4e1cf68ff23a5d7fe0050894facecf1
tlwr <tlornewr@gmail.com>
2014-04-09 00:41:30 -0400

    change from retardation

--- app/models/move.rb
+++ app/models/move.rb
@@ -712,12 +712,13 @@ class Move < ActiveRecord::Base
       game.player_1.save
       game.player_2.wins += 1
       game.player_2.save
+      game.delete
     elsif game.ships.length{|ship| ship.turn == 1}.length == 0
       game.player_2.losses += 1
       game.player_2.save
       game.player_1.wins += 1
       game.player_1.save
-    end
     game.delete
+    end
   end
 end
